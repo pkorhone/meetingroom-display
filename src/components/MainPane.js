fd97f0195d50aebee8dc65c2af94d7ac50d34fe5
pkorhone <pekka.korhonen@outlook.com>
2020-02-05 13:50:46 +0200

    restructured

--- src/components/MainPane.js
+++ src/components/MainPane.js
@@ -11,14 +11,28 @@ const MainPane = ({ meetings }) => {
   const now = new Date(moment()._d)
 
   useEffect(() => {
+    // find currently ongoing meeting
     meetings.forEach(meeting => {
       if (eventInProgress(meeting)) {
         setCurrentMeeting(meeting)
       }
     })
+
+    //find upcoming meetings (max. 3)
     findNextMeetings()
   }, [])
 
+  const findNextMeetings = () => {
+    // find all meetings in the future and sort by start time
+    const futureMeetings = meetings.filter(meeting => !eventInProgress(meeting) && eventStartsLater(meeting))
+    futureMeetings.sort((a, b) => new Date(a.StartTime) - new Date(b.StartTime))
+    // only show the next 3 upcoming meetings
+    const nextThree = futureMeetings.filter((meeting, index, array) => index < 3)
+    setNextMeetings(nextThree)
+  }
+
+
+  // check if event is in progress
   const eventInProgress = (event) => {
     const startTime = new Date(event.StartTime)
     const endTime = new Date(event.EndTime)
@@ -33,6 +47,8 @@ const MainPane = ({ meetings }) => {
     return false
   }
 
+  
+  // check if event start time is in the future
   const eventStartsLater = (event) => {
     const startTime = new Date(event.StartTime)
     if (startTime > now) {
@@ -41,15 +57,6 @@ const MainPane = ({ meetings }) => {
     }
     // event has already started
     return false
-  }
-
-  const findNextMeetings = () => {
-    // find all meetings in the future and sort by start time
-    const futureMeetings = meetings.filter(meeting => !eventInProgress(meeting) && eventStartsLater(meeting))
-    futureMeetings.sort((a, b) => new Date(a.StartTime) - new Date(b.StartTime))
-    // only show the next 3 upcoming meetings
-    const nextThree = futureMeetings.filter((meeting, index, array) => index < 3)
-    setNextMeetings(nextThree)
   }
 
   return (
