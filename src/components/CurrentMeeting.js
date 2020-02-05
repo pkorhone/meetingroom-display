37a868882dd59631472433cf00d72bb4ca79456f
pkorhone <pekka.korhonen@outlook.com>
2020-02-05 12:41:41 +0200

    logic moved from CurrentMeeting to parent component MainPane

--- src/components/CurrentMeeting.js
+++ src/components/CurrentMeeting.js
@@ -1,33 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import moment from 'moment'
+import React from 'react'
 import './CurrentMeeting.css'
 
-const CurrentMeeting = ({ meetings }) => {
-
-  const [currentMeeting, setCurrentMeeting] = useState(null)
-  const now = moment()._d
-
-  useEffect(() => {
-    meetings.forEach(meeting => {
-      if (eventInProgress(meeting)) {
-        setCurrentMeeting(meeting)
-      }
-    })
-  }, [])
-
-  const eventInProgress = (event) => {
-    const startTime = new Date(event.StartTime)
-    const endTime = new Date(event.EndTime)
-    const hasStarted = startTime < now
-    const hasEnded = endTime < now
-
-    if (hasStarted && !hasEnded) {
-      // event has started but has not yet ended
-      return true
-    }
-    // any other case
-    return false
-  }
+const CurrentMeeting = ({ currentMeeting }) => {
 
   return (
     <div className="main">
--- src/components/MainPane.js
+++ src/components/MainPane.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
 import CurrentMeeting from './CurrentMeeting'
 import NextMeetings from './NextMeetings'
 import './MainPane.css'
 
 const MainPane = ({ meetings }) => {
+
+  const [currentMeeting, setCurrentMeeting] = useState(null)
+  const now = moment()._d
+
+  useEffect(() => {
+    meetings.forEach(meeting => {
+      if (eventInProgress(meeting)) {
+        setCurrentMeeting(meeting)
+      }
+    })
+  }, [])
+
+  const eventInProgress = (event) => {
+    const startTime = new Date(event.StartTime)
+    const endTime = new Date(event.EndTime)
+    const hasStarted = startTime < now
+    const hasEnded = endTime < now
+
+    if (hasStarted && !hasEnded) {
+      // event has started but has not yet ended
+      return true
+    }
+    // any other case
+    return false
+  }
+
   return (
     <div className='mainPane'>
-      <CurrentMeeting meetings={meetings}/>
+      <CurrentMeeting currentMeeting={currentMeeting}/>
       <NextMeetings meetings={meetings}/>
     </div>
   )
