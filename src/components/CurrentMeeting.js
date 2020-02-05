ff8c6033a409e82d68c750be0414ba11d4c952fb
pkorhone <pekka.korhonen@outlook.com>
2020-02-05 11:22:42 +0200

    current meeting font weights

--- src/components/CurrentMeeting.js
+++ src/components/CurrentMeeting.js
@@ -4,10 +4,10 @@ import './CurrentMeeting.css'
 const CurrentMeeting = (props) => {
   return (
     <div className="main">
-      <p>CURRENT MEETING</p>
-      <h1>ECRAFT MEETING TITLE</h1>
+      <h3 className='subTitle'>CURRENT MEETING</h3>
+      <h1 className='hugeTitle'>ECRAFT MANAGEMENT <br />MONTHLY MEETING</h1>
       <p>| ---------------- Timeline here ---------------- |</p>
-      <p>ORGANIZER NAME</p>
+      <h3 className='subTitle'>ORGANIZER NAME</h3>
     </div>
   )
 }
