e434bdd5990d00e3a3d88d35aecec5de3293e194
pkorhone <pekka.korhonen@outlook.com>
2020-02-05 18:35:49 +0200

    icons & collapse participants

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,36 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Icon } from 'semantic-ui-react'
 import './Detail.css'
 
 const Detail = (props) => {
+
+  const [collapsed, setCollapsed] = useState(true)
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed)
+  }
+
+  const collapsibleContent = () => {
+    if (collapsed) {
+      return null;
+    }
+    return (
+      <div>
+        {props.content.map(participant => 
+          <div key={participant.Name + participant.Title} className='contentRow'>
+            <h5 className='participantName'>{participant.Name.toUpperCase()}</h5>
+            <h5 className='participantTitle'>{participant.Title.toUpperCase()}</h5>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+
   return (
     <div>
 
       <div className='detailTitleBar'>
         <div className='detailIcon'>
-          <p>X</p>
+          <img src={props.icon} alt=''/>
         </div>
         <div className='detailTitle'>
           <p>{props.title}</p>
         </div>
-        <div className='detailCollapse'>
-          <p>X</p>
-        </div>
+        {props.type === 'collapse' ? 
+          // show collapse icon for collapsible content
+          <div className='detailCollapse' onClick={() => toggleCollapsed()}>
+            {collapsed ? <Icon name='chevron down' size='large'/> : <Icon name='chevron up' size='large'/>}
+          </div> :
+          null
+        }
+        
       </div>
 
       {props.type ?
         props.type === 'collapse' ?
-        //collapse content
-          <div>
-            {props.content.map(participant => 
-              <div key={participant.Name + participant.Title} className='contentRow'>
-                <h5 className='participantName'>{participant.Name.toUpperCase()}</h5>
-                <h5 className='participantTitle'>{participant.Title.toUpperCase()}</h5>
-              </div>
-            )}
-          </div> :
-          // show content
+
+        // collapsible content (Array)
+        
+          collapsibleContent() :
+
+          // regular content (String)
           <div className='contentRow'>
-            <p>{props.content}</p>
+            <p className='description'>{props.content}</p>
           </div>
         :
         // no content
--- src/components/EventDetailsView.js
+++ src/components/EventDetailsView.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './EventDetailsView.css'
 import Detail from './Detail'
+import { Icon } from 'semantic-ui-react'
+import clockIcon from '../icons/time.png'
+import personIcon from '../icons/person.png'
+import descIcon from '../icons/description.png'
 
 const EventDetailsView = ({ meeting, onReturnToCalendar }) => {
 
@@ -14,17 +18,22 @@ const EventDetailsView = ({ meeting, onReturnToCalendar }) => {
     'Saturday'
   ]
 
+  // parse meeting start & end from JSON data to Date objects
   const startDateObj = new Date(meeting.StartTime)
   const endDateObj = new Date(meeting.EndTime)
 
+
+  // get presentable dates in format DD.MM.YYYY
   const startDate = `${startDateObj.getDate()}.${startDateObj.getMonth()+1}.${startDateObj.getFullYear()}`
   const endDate = `${endDateObj.getDate()}.${endDateObj.getMonth()+1}.${endDateObj.getFullYear()}`
   
+
   const singleDayEvent = () => {
     //returns true if event starts and ends on the same date.
     return startDate === endDate
   }
 
+  // get event date for display in event details (e.g. "Thursday, 6.2.2020")
   const eventDate = () => {
     if (singleDayEvent()) {
       // case 1: event starts and ends on the same day
@@ -34,7 +43,10 @@ const EventDetailsView = ({ meeting, onReturnToCalendar }) => {
     return `${weekdays[startDateObj.getDay()]}, ${startDate} - \n${weekdays[endDateObj.getDay()]}, ${endDate}`
   }
 
+  // get event length for display in event details (e.g. "9.30 to 10.30")
   const eventLength = () => {
+    
+    // helper method to get presentable time label (format HH.MM)
     const timeString = (time) => {
       const hrs = time.getHours() === 0 ? '00' : time.getHours()
       const mins = time.getMinutes() === 0 ? '00' : time.getMinutes()
@@ -57,18 +69,18 @@ const EventDetailsView = ({ meeting, onReturnToCalendar }) => {
     <div>
       <div className='eventTitleBar'>
         <div className='backButton' onClick={() => onReturnToCalendar()}>
-          <p>X</p>
+          <Icon inverted name='chevron left' size='large'/>
         </div>
         <div className='eventTitle'>
-          <p>{meeting.Subject}</p>
+          <h3>{meeting.Subject}</h3>
         </div>
       </div>
 
-      <Detail title={eventDate()} />
-      <Detail title={eventLength()} />
+      <Detail title={eventDate().toUpperCase()} icon={clockIcon}/>
+      <Detail title={eventLength().toUpperCase()} icon={clockIcon}/>
       {/* optional properties Participants & Description only rendered if present */}
-      {!meeting.hasOwnProperty('Participants') || meeting.Participants === null ? null : <Detail title='Participants' type='collapse' content={meeting.Participants}/> }
-      {!meeting.hasOwnProperty('Description') || meeting.Description === null ? null : <Detail title='Description' type='show' content={meeting.Description}/> }
+      {!meeting.hasOwnProperty('Participants') || meeting.Participants === null ? null : <Detail title='PARTICIPANTS' type='collapse' content={meeting.Participants} icon={personIcon}/> }
+      {!meeting.hasOwnProperty('Description') || meeting.Description === null ? null : <Detail title='DESCRIPTION' type='show' content={meeting.Description} icon={descIcon} /> }
     </div>
   )
 }
